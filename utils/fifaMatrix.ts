cdad56d78fdc68315587814cda456971779167ab
pantem <seshomaru@gmail.com>
2026-06-01 19:01:54 -0600

    cruces en todas las fases 4

--- utils/fifaMatrix.ts
+++ utils/fifaMatrix.ts
@@ -87,26 +87,22 @@ function getKnockoutSlots(): KnockoutSlot[] {
 
 function getR32Slots(): KnockoutSlot[] {
   return [
-    { matchId: "R32_01", round: "r32", homeLabel: "1A", awayLabel: "3C/D/E", homeSource: "1A", awaySource: "3C", label: "R32-1" },
-    { matchId: "R32_02", round: "r32", homeLabel: "1B", awayLabel: "3A/C/D", homeSource: "1B", awaySource: "3A", label: "R32-2" },
-    { matchId: "R32_03", round: "r32", homeLabel: "1C", awayLabel: "3D/E/F", homeSource: "1C", awaySource: "3D", label: "R32-3" },
-    { matchId: "R32_04", round: "r32", homeLabel: "1D", awayLabel: "3E/F/G", homeSource: "1D", awaySource: "3E", label: "R32-4" },
-    { matchId: "R32_05", round: "r32", homeLabel: "1E", awayLabel: "3F/G/H", homeSource: "1E", awaySource: "3F", label: "R32-5" },
-    { matchId: "R32_06", round: "r32", homeLabel: "1F", awayLabel: "3G/H/I", homeSource: "1F", awaySource: "3G", label: "R32-6" },
-    { matchId: "R32_07", round: "r32", homeLabel: "1G", awayLabel: "3H/I/J", homeSource: "1G", awaySource: "3H", label: "R32-7" },
-    { matchId: "R32_08", round: "r32", homeLabel: "1H", awayLabel: "3I/J/K", homeSource: "1H", awaySource: "3I", label: "R32-8" },
-    { matchId: "R32_09", round: "r32", homeLabel: "1I", awayLabel: "3J/K/L", homeSource: "1I", awaySource: "3J", label: "R32-9" },
-    { matchId: "R32_10", round: "r32", homeLabel: "1J", awayLabel: "3K/L/A", homeSource: "1J", awaySource: "3K", label: "R32-10" },
-    { matchId: "R32_11", round: "r32", homeLabel: "1K", awayLabel: "3L/A/B", homeSource: "1K", awaySource: "3L", label: "R32-11" },
-    { matchId: "R32_12", round: "r32", homeLabel: "1L", awayLabel: "3B/C/D", homeSource: "1L", awaySource: "3B", label: "R32-12" },
+    { matchId: "R32_01", round: "r32", homeLabel: "1A", awayLabel: "3C", homeSource: "1A", awaySource: "3C", label: "R32-1" },
+    { matchId: "R32_02", round: "r32", homeLabel: "1B", awayLabel: "3A", homeSource: "1B", awaySource: "3A", label: "R32-2" },
+    { matchId: "R32_03", round: "r32", homeLabel: "1C", awayLabel: "3D", homeSource: "1C", awaySource: "3D", label: "R32-3" },
+    { matchId: "R32_04", round: "r32", homeLabel: "1D", awayLabel: "3E", homeSource: "1D", awaySource: "3E", label: "R32-4" },
+    { matchId: "R32_05", round: "r32", homeLabel: "1E", awayLabel: "3F", homeSource: "1E", awaySource: "3F", label: "R32-5" },
+    { matchId: "R32_06", round: "r32", homeLabel: "1F", awayLabel: "3G", homeSource: "1F", awaySource: "3G", label: "R32-6" },
+    { matchId: "R32_07", round: "r32", homeLabel: "1G", awayLabel: "3H", homeSource: "1G", awaySource: "3H", label: "R32-7" },
+    { matchId: "R32_08", round: "r32", homeLabel: "1H", awayLabel: "3I", homeSource: "1H", awaySource: "3I", label: "R32-8" },
+    { matchId: "R32_09", round: "r32", homeLabel: "1I", awayLabel: "2J", homeSource: "1I", awaySource: "2J", label: "R32-9" },
+    { matchId: "R32_10", round: "r32", homeLabel: "1J", awayLabel: "2K", homeSource: "1J", awaySource: "2K", label: "R32-10" },
+    { matchId: "R32_11", round: "r32", homeLabel: "1K", awayLabel: "2L", homeSource: "1K", awaySource: "2L", label: "R32-11" },
+    { matchId: "R32_12", round: "r32", homeLabel: "1L", awayLabel: "2I", homeSource: "1L", awaySource: "2I", label: "R32-12" },
     { matchId: "R32_13", round: "r32", homeLabel: "2A", awayLabel: "2B", homeSource: "2A", awaySource: "2B", label: "R32-13" },
     { matchId: "R32_14", round: "r32", homeLabel: "2C", awayLabel: "2D", homeSource: "2C", awaySource: "2D", label: "R32-14" },
     { matchId: "R32_15", round: "r32", homeLabel: "2E", awayLabel: "2F", homeSource: "2E", awaySource: "2F", label: "R32-15" },
     { matchId: "R32_16", round: "r32", homeLabel: "2G", awayLabel: "2H", homeSource: "2G", awaySource: "2H", label: "R32-16" },
-    { matchId: "R32_17", round: "r32", homeLabel: "2I", awayLabel: "2J", homeSource: "2I", awaySource: "2J", label: "R32-17" },
-    { matchId: "R32_18", round: "r32", homeLabel: "2K", awayLabel: "2L", homeSource: "2K", awaySource: "2L", label: "R32-18" },
-    { matchId: "R32_19", round: "r32", homeLabel: "3A", awayLabel: "3B", homeSource: "3A", awaySource: "3B", label: "R32-19" },
-    { matchId: "R32_20", round: "r32", homeLabel: "3C", awayLabel: "3D", homeSource: "3C", awaySource: "3D", label: "R32-20" },
   ]
 }
 
@@ -120,31 +116,28 @@ function getR16Slots(): KnockoutSlot[] {
     { matchId: "R16_06", round: "r16", homeLabel: "W R32-11", awayLabel: "W R32-12", homeSource: "W11", awaySource: "W12", label: "R16-6" },
     { matchId: "R16_07", round: "r16", homeLabel: "W R32-13", awayLabel: "W R32-14", homeSource: "W13", awaySource: "W14", label: "R16-7" },
     { matchId: "R16_08", round: "r16", homeLabel: "W R32-15", awayLabel: "W R32-16", homeSource: "W15", awaySource: "W16", label: "R16-8" },
-    { matchId: "R16_09", round: "r16", homeLabel: "W R32-17", awayLabel: "W R32-18", homeSource: "W17", awaySource: "W18", label: "R16-9" },
-    { matchId: "R16_10", round: "r16", homeLabel: "W R32-19", awayLabel: "W R32-20", homeSource: "W19", awaySource: "W20", label: "R16-10" },
   ]
 }
 
 function getQfSlots(): KnockoutSlot[] {
   return [
-    { matchId: "QF_01", round: "qf", homeLabel: "W R16-1", awayLabel: "W R16-2", homeSource: "W21", awaySource: "W22", label: "QF-1" },
-    { matchId: "QF_02", round: "qf", homeLabel: "W R16-3", awayLabel: "W R16-4", homeSource: "W23", awaySource: "W24", label: "QF-2" },
-    { matchId: "QF_03", round: "qf", homeLabel: "W R16-5", awayLabel: "W R16-6", homeSource: "W25", awaySource: "W26", label: "QF-3" },
-    { matchId: "QF_04", round: "qf", homeLabel: "W R16-7", awayLabel: "W R16-8", homeSource: "W27", awaySource: "W28", label: "QF-4" },
-    { matchId: "QF_05", round: "qf", homeLabel: "W R16-9", awayLabel: "W R16-10", homeSource: "W29", awaySource: "W30", label: "QF-5" },
+    { matchId: "QF_01", round: "qf", homeLabel: "W R16-1", awayLabel: "W R16-2", homeSource: "W17", awaySource: "W18", label: "QF-1" },
+    { matchId: "QF_02", round: "qf", homeLabel: "W R16-3", awayLabel: "W R16-4", homeSource: "W19", awaySource: "W20", label: "QF-2" },
+    { matchId: "QF_03", round: "qf", homeLabel: "W R16-5", awayLabel: "W R16-6", homeSource: "W21", awaySource: "W22", label: "QF-3" },
+    { matchId: "QF_04", round: "qf", homeLabel: "W R16-7", awayLabel: "W R16-8", homeSource: "W23", awaySource: "W24", label: "QF-4" },
   ]
 }
 
 function getSfSlots(): KnockoutSlot[] {
   return [
-    { matchId: "SF_01", round: "sf", homeLabel: "W QF-1", awayLabel: "W QF-2", homeSource: "W31", awaySource: "W32", label: "SF-1" },
-    { matchId: "SF_02", round: "sf", homeLabel: "W QF-3", awayLabel: "W QF-4", homeSource: "W33", awaySource: "W34", label: "SF-2" },
+    { matchId: "SF_01", round: "sf", homeLabel: "W QF-1", awayLabel: "W QF-2", homeSource: "W25", awaySource: "W26", label: "SF-1" },
+    { matchId: "SF_02", round: "sf", homeLabel: "W QF-3", awayLabel: "W QF-4", homeSource: "W27", awaySource: "W28", label: "SF-2" },
   ]
 }
 
 function getFinalSlots(): KnockoutSlot[] {
   return [
-    { matchId: "F_01", round: "final", homeLabel: "W SF-1", awayLabel: "W SF-2", homeSource: "W35", awaySource: "W36", label: "Final" },
+    { matchId: "F_01", round: "final", homeLabel: "W SF-1", awayLabel: "W SF-2", homeSource: "W29", awaySource: "W30", label: "Final" },
   ]
 }
 
@@ -153,9 +146,9 @@ export function getMatchesByRound(matches: KnockoutMatch[], round: string): Knoc
 }
 
 const PROPAGATION: [string, number, string, number][] = [
-  ["r32", 20, "r16", 10],
-  ["r16", 10, "qf", 5],
-  ["qf", 5, "sf", 2],
+  ["r32", 16, "r16", 8],
+  ["r16", 8, "qf", 4],
+  ["qf", 4, "sf", 2],
   ["sf", 2, "final", 1],
 ]
 
